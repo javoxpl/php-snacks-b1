441f131fe95fe0aae32f867b9c3d381db18645bd
Lorenzo Iacovone <99271423+javoxpl@users.noreply.github.com>
2022-07-08 18:11:46 +0200

    completed

--- snack4/index.php
+++ snack4/index.php
@@ -1,2 +1,16 @@
 /* Creare un array con 15 numeri casuali, tenendo conto che l’array 
 non dovrà contenere lo stesso numero più di una volta */
+
+
+<?php
+
+$numArray = [];
+
+while (count($numArray) < 15){
+    $randomNum = rand(1, 100);
+    if (!in_array($randomNum, $numArray)){
+        $numArray[] = $randomNum;
+    }
+}
+
+var_dump($numArray);
